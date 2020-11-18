f0cdb27775e1d8a0721e5893999d4524c69e24df
Brandon <bmehlenbacher4@gmail.com>
2020-11-17 20:33:09 -0600

    Added sort and parse functions

--- FiberCavities/FiberDatabase.py
+++ FiberCavities/FiberDatabase.py
@@ -3,10 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-class FibersDatabase(object):
+class FiberDatabase(object):
     def __init__(self,fileLocation=r"C:\Users\bmehl\Desktop\Fibers_Database.xlsx",sheetName= "2019"):
         self.database = pd.read_excel(fileLocation,sheet_name=sheetName)
+        self.database.set_index('Label',inplace=True)
         self.database['NA'] = self.database['Dia-Avg']/(self.database['ROC-Avg'])
+        self.specificFiber = None
         self.fibersRemoved()
         self._smallestROC = self.database['ROC-Avg'].min()
         self._largestROC = self.database['ROC-Avg'].max()
@@ -14,9 +16,22 @@ class FibersDatabase(object):
         self._largestDia = self.database['Dia-Avg'].max()
         self._smallestEllipticity = self.database['Ellipticity'].min()
 
-    def minimumROC(self):
+    def smallestROC(self):
         return self._smallestROC
 
+    def largestROC(self):
+        return self._largestROC
+
+    def smallestDia(self):
+        return self._smallestDia
+
+    def largestDia(self):
+        return self._largestDia
+
+    def getFiberData(self,fiber="P11F2"):
+        self.specificFiber = self.database.loc[fiber]
+        return self.specificFiber
+    
     def acceptableNA(self,NA):
         """
         The idea of this function is to identify the fibers which
@@ -45,13 +60,16 @@ class FibersDatabase(object):
         """
         self.database = self.database[pd.isnull(self.database['Date Removed'])]
 
+    def sortDatabase(self,column = "Fiber Type",ascending=True):
+        self.database.sort_values([column],ascending=ascending,inplace=True)
+    
     def head(self,entries=5):
         print(self.database.head(entries))
 
-    def head(self,entries=5):
+    def tail(self,entries=5):
         print(self.database.tail(entries))
 
 if __name__ == "__main__":
-    database = FibersDatabase()
+    database = FiberDatabase()
     print(database.database.head())
-    database.minimumROC()
+    print(database.getFiberData())
